74d8d94830ef10fb8c08049a31ce1debb14d32e0
COMLAB <oprincipe23-0385@cca.edu.ph>
2025-03-05 19:21:53 +0800

    container olit

--- container.php
+++ container.php
@@ -1,4 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
 <div class="container">
   <h1>My First Bootstrap Page</h1>
-  <p>This is some text.</p>
+  <p>This part is inside a .container class.</p>
+  <p>The .container class provides a responsive fixed width container.</p>
 </div>
+
+<div class="container">
+<p class="h1">h1 Bootstrap heading</p>
+<p class="h2">h2 Bootstrap heading</p>
+<p class="h3">h3 Bootstrap heading</p>
+<p class="h4">h4 Bootstrap heading</p>
+<p class="h5">h5 Bootstrap heading</p>
+<p class="h6">h6 Bootstrap heading</p
+
+</div>  
+<div class="container">
+<button type="button" class="btn">Basic</button>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-dark">Dark</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-link">Link</button>
+</div>
+
+</body>
+</html>
